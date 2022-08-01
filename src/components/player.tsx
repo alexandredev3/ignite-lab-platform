817511eb9b8d89e5792401456eabd14eb5389a46
Alexandre <costasalexandre511@gmail.com>
2022-08-01 08:42:01 -0300

    fix player autoplay switch style

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -111,13 +111,13 @@ export function Player({ videoId, lessonId }: PlayerProps) {
               checked={autoPlayEnabled}
               onChange={handleToggleAutoPlay}
               className={`${autoPlayEnabled ? 'bg-green-500' : 'bg-gray-600'}
-                relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                relative inline-flex h-[32px] w-[64px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
             >
               <span className="sr-only">Autoplay</span>
               <span
                 aria-hidden="true"
-                className={`${autoPlayEnabled ? 'translate-x-9' : 'translate-x-0'}
-                  pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                className={`${autoPlayEnabled ? 'translate-x-8' : 'translate-x-0'}
+                  pointer-events-none inline-block h-[28px] w-[28px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
               />
             </Switch>
           </div>
